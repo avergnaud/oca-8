0b7878ff9e666c2de257deb8e8d5811506a93575
avergnaud <a.vergnaud@gmail.com>
2015-09-12 14:32:03 +0200

    init

--- stringmethods/Indexes.java
+++ stringmethods/Indexes.java
@@ -1,6 +1,9 @@
 package stringmethods;
 public class Indexes {
 	public static void main(String... args) {
-		
+		String animals = "animals";
+		int test = animals.indexOf('s',7);
+		System.out.println(test);
+		System.out.println(animals.charAt(7));
 	}
 }
